b206f11f9f005d4e0998774c7686b951caf07f0a
cwuestenhagen <33098140+cwuestenhagen@users.noreply.github.com>
2019-07-01 13:51:31 +0200

    final_version

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 # Exercise 6
 from pathlib import Path
+import satpy as satpy
+import utils as ut
 
-input_dir  = Path("data")
+#%%
 
 # 1. Read the Scene that you downloaded from the data directory using SatPy. [2P]
 
+files = satpy.find_files_and_readers(base_dir= "./data", reader="seviri_l1b_nc")
+
+scn = satpy.Scene(filenames=files)
+scn.attrs
+scn.all_dataset_names()
+
+#%%
+
 # 2. Load the composites "natural_color" and "convection" [2P]
+
+scn.available_composite_names()
+scn.load(["natural_color"])
+#scn.show("natural_color")
+scn.load(["convection"])
+#scn.show("convection")
+
+#%%
 
 # 3. Resample the fulldisk to the Dem. Rep. Kongo and its neighbours [4P] 
 #    by defining your own area in Lambert Azimuthal Equal Area. 
@@ -15,4 +33,16 @@ input_dir  = Path("data")
 #      - projection x/y coordinates of lower left: -15E5
 #      - projection x/y coordinates of upper right: 15E5 
 
+
+ut.resample_area(scn, projection="laea", lat_origin =-3, lon_origin=23, width=500, height=500,
+                  llx=-15E5, lly=-15E5 , urx=15E5, ury=15E5, saveFile="natural_color_Kongo.png",savePath = "./output", scene="natural_color",
+                  area_id = "Demokratische Republik Kongo" , description = "Demokratische Republik Kongo in der Lambert Azimuthal Equal Area-Projektion", proj_id = "Demokratische Republik Kongo")
+
+ut.resample_area(scn, projection="laea", lat_origin =-3, lon_origin=23, width=500, height=500,
+                  llx=-15E5, lly=-15E5 , urx=15E5, ury=15E5, saveFile="convection_Kongo.png",savePath = "./output", scene="convection",
+                  area_id = "Demokratische Republik Kongo" , description = "Demokratische Republik Kongo in der Lambert Azimuthal Equal Area-Projektion", proj_id = "Demokratische Republik Kongo")
+
+#%%
 # 4. Save both loaded composites of the resampled Scene as simple png images. [2P]
+
+# look at the function of task 3
--- utils.py
+++ utils.py
@@ -2,3 +2,52 @@
 
 # Use this file for all functions you create and might want to reuse later.
 # Import them in the `main.py` script
+
+def resample_area(scn, projection, lat_origin, lon_origin, width, height,
+                  llx, lly, urx, ury, saveFile, savePath, scene,
+                  area_id = "NA" , description = "NA", proj_id = "NA"):
+    """
+    resample a scene to a specific area and projection
+    
+    Parameters
+    ----------
+    scn : scene
+    projection : projection of the loaded scene
+    lat_origin : latitude of origin
+    lon_origin : longitude of origin
+    width : width of the resampled scene
+    height : height of the resampled scene
+    llx : projection x coordinate of lower left
+    lly : projection y coordinate of lower left
+    urx : projection x coordinate of upper right
+    ury : projection y coordinate of upper right
+    saveFile : name of the produced image
+    savePath : path to the directory for produced image 
+    scene : specific composit 
+    area_id : id of the area
+    description : description of the resampled area
+    proj_id : id of the project
+        
+    Returns
+    -------
+    
+    
+    """
+    from pyresample.geometry import AreaDefinition
+    scn.load([scene])
+    proj_dict = {"proj": projection, "lat_ts": lat_origin, "lon_0": lon_origin}
+    area_extent = (llx,lly,urx,ury)
+    area_def = AreaDefinition(area_id, proj_id, description, proj_dict, width, height, area_extent)
+    local_scn = scn.resample(area_def)
+    local_scn.save_dataset(dataset_id=scene,
+                           writer="simple_image",
+                           filename= saveFile,
+                           base_dir= savePath)
+    
+
+
+
+
+        
+
+
